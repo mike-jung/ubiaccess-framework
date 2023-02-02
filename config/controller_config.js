5fb6b307e5db7f5b3d4479f4549da8091bf38172
mike-jung <mike.jung.global@gmail.com>
2023-02-02 10:47:14 +0900

    Update

--- config/controller_config.js
+++ config/controller_config.js
@@ -3,6 +3,6 @@ module.exports = [
     {id:3, type:'path', path:'/profile/get', method:['get','post'], file:'profile-controller', func:'get'},
     {id:4, type:'path', path:'/profile/add', method:['get'], file:'profile-controller', func:'add'},
     {id:5, type:'path', path:'/profile/upload', method:['post'], file:'profile-controller', func:'upload', upload:true},
-    {id:6, type:'path', path:'/test/set_cookie', method:['get'], file:'test-controller', func:'set_cookie'},
-    {id:7, type:'path', path:'/test/show_cookie', method:['get'], file:'test-controller', func:'show_cookie'}
+    {id:6, type:'path', path:'/cookie/set_cookie', method:['get'], file:'cookie-controller', func:'set_cookie'},
+    {id:7, type:'path', path:'/cookie/show_cookie', method:['get'], file:'cookie-controller', func:'show_cookie'}
 ];
